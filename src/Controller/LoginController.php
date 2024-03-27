5b1cb720b91dd1ef56beb075dae41c77c8d3e882
Sébastien <slopez@et.intechinfo.fr>
2024-03-27 14:02:27 +0100

    Try Correcting Branches

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LoginController extends AbstractController
+{
+    private $entityManager;
+    private $repository;
+
+    public function __contruct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+        $this->repository = $entityManager->getRepository(User::class);
+    }
+    // pointless but need to test something
+    #[Route('/login', name: 'app_login', methods: 'GET')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/LoginController.php',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login_post', methods: ['POST', 'PUT'])]
+    public function login(Request $request): JsonResponse
+    {
+        $user = $this->repository->findOneBy(["email"=>"slopez@orange.fr"]);
+        return $this->json([
+            'user' => json_encode($user),
+            'data' => $request->getContent(),
+            'message' => 'Welcome to your new controller!', 
+            'path' => 'src/Controller/LoginController.php',
+        ]);
+    }
+}
