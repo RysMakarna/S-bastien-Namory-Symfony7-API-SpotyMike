6a399d5084c640a8bdd3ae3c0ba1ce9c38d074d8
Namory <nnamorykanate5@gmail.com>
2024-04-11 14:41:08 +0200

    ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class UserController extends AbstractController
 {
@@ -118,6 +119,50 @@ class UserController extends AbstractController
             'message' => 'aucun utilisateur pour le moment',
         ], 204);
 
+    }
+    #[Route('/password-lost', name: 'app_read_user')]
+    public function DeleteAcount(Request $request): JsonResponse
+    {
+        $email_validation_regex = '/^\\S+@\\S+\\.\\S+$/';
+        $email = $request->get('email');
+
+        if(empty($email)){
+            return $this->json([
+                'error'=>true,
+                'message'=> 'L\email manquant.Veuillez fornir votre mail pour la récupération du mot de passe.'
+            ],400);
+        }
+
+        if(!preg_match($email_validation_regex,$email)){
+            return $this->json([
+            'error'=>true,
+            'message'=> 'Le format de l \'email est invalide.Veuillez entrer un email valide'
+            ],400);
+        }
+        //verifier si l'utilisateur à un email
+        $current_user = $this->repository->findOneBy(['email'=> $email]);
+        if($current_user == null){
+            return $this->json([
+                'error'=>true,
+                'message'=> 'Aucun compte  n\'est associé à cet email.Veuillez  vérifier et réssayer'
+            ],404);
+        }
+        $cache = new FilesystemAdapter();
+        $cacheKey = 'reset_password_' . urlencode($email);
+        $cacheItem = $cache->getItem($cacheKey);
+        $requestCount = $cacheItem->get() ?? 0;
+ 
+ 
+        $cacheItem->set($requestCount + 1);
+        $cacheItem->expiresAfter(5); // 20 seconds
+        $cache->save($cacheItem);
+
+
+
+        return $this->json([
+            'message' => 'Un email de réinitialisation de mot de passe à été envoyé à votre adresse email.
+            Veuillez suivre les instructions contenues dans l\'email pour réinitialiser votre mot de passe.',
+        ], 200);
     }
 
     #[Route('/update/user/{id}', name: 'app_update_user', methods: ['PUT'])]
